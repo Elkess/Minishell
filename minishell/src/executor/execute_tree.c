7612ce590ca907aea2635555b28a08783c30e8aa
Mohammed Amine Elkess <melkess@e3r8p1.1337.ma>
2025-06-18 10:17:59 +0100

    last push

--- minishell/src/executor/execute_tree.c
+++ minishell/src/executor/execute_tree.c
@@ -6,7 +6,7 @@
 /*   By: melkess <melkess@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 08:39:55 by melkess           #+#    #+#             */
-/*   Updated: 2025/06/17 14:11:25 by melkess          ###   ########.fr       */
+/*   Updated: 2025/06/18 10:15:22 by melkess          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,37 @@ t_pid	*add_to_pids_list(t_pid *head, int val)
 	}
 	return (backup);
 }
+int can_we_fork(t_tree *tree)
+{
+	pid_t pid;
+
+	if (!tree)
+		return (1);
+	if (tree->type == NODE_PIPE)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("Fork failed during can_we_fork check");
+			return (0);
+		}
+		if (pid == 0)
+		{
+			int left_ok = can_we_fork(tree->left);
+			int right_ok = can_we_fork(tree->right);
+			exit(!(left_ok && right_ok));
+		}
+		else
+		{
+			int status;
+			waitpid(pid, &status, 0);
+			if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+				return (0);
+		}
+	}
+	return (1);
+}
+
 
 int	execute_pipes(t_tree *tree, t_env **envh, t_tool	*tool)
 {
@@ -49,68 +80,71 @@ int	execute_pipes(t_tree *tree, t_env **envh, t_tool	*tool)
 	int				exit_stat;
 	t_pid			*tmp;
 
-	if (tree->type != NODE_PIPE)
-		return (-1);
-	tool->inside_pipe = 1;
-	if (pipe(pipefd) == -1)
-		return (perror("Pipe error"), 1);
-	pids[0] = fork();
-	if (pids[0] > 0)
-		pid = add_to_pids_list(pid, pids[0]);
-	if (pids[0] == -1)
+	if (can_we_fork(tree))
 	{
-		perror("Fork failed");
-		while (pid)
+		if (tree->type != NODE_PIPE)
+			return (-1);
+		tool->inside_pipe = 1;
+		if (pipe(pipefd) == -1)
+			return (perror("Pipe error"), 1);
+		pids[0] = fork();
+		if (pids[0] > 0)
+			pid = add_to_pids_list(pid, pids[0]);
+		if (pids[0] == -1)
 		{
-			kill(pid->value, SIGKILL);
-			tmp = pid;
-			pid = pid->next;
-			free(pid);
-			pid = NULL;
+			perror("Fork failed");
+			while (pid)
+			{
+				kill(pid->value, SIGKILL);
+				tmp = pid;
+				pid = pid->next;
+				free(pid);
+				pid = NULL;
+			}
+			return (1);
 		}
-		return (1);
-	}
-	if (pids[0] == 0)
-	{
-		(close(pipefd[0]), dup2(pipefd[1], 1));
-		(close(pipefd[1]), exit(execute_tree(tree->left, envh, tool))); // SHoud it be exit and free_ evnh ???
-	}
-	tool->err = 0;
-	pids[1] = fork();
-	if (pids[1] == -1)
-	{		perror("Fork failed");
-		while (pid)
+		if (pids[0] == 0)
 		{
-			kill(pid->value, SIGKILL);
-			tmp = pid;
-			pid = pid->next;
-			free(pid);
-			pid = NULL;
+			(close(pipefd[0]), dup2(pipefd[1], 1));
+			(close(pipefd[1]), exit(execute_tree(tree->left, envh, tool))); // SHoud it be exit and free_ evnh ???
 		}
-		return (1);
+		tool->err = 0;
+		pids[1] = fork();
+		if (pids[1] == -1)
+		{		perror("Fork failed");
+			while (pid)
+			{
+				kill(pid->value, SIGKILL);
+				tmp = pid;
+				pid = pid->next;
+				free(pid);
+				pid = NULL;
+			}
+			return (1);
+		}
+		if (pids[1] > 0)
+			pid = add_to_pids_list(pid, pids[1]);
+		if (pids[1] == 0)
+		{
+			(close(pipefd[1]), dup2(pipefd[0], 0));
+			(close(pipefd[0]), exit(execute_tree(tree->right, envh, tool))); // SHoud it be exit and free_ evnh
+		}
+		signal(SIGINT, SIG_IGN);
+		(close(pipefd[0]), close(pipefd[1]));
+		(waitpid(pids[0], &status[0], 0), waitpid(pids[1], &status[1], 0));
+		if(WIFSIGNALED(status[0]))
+			tool->signal = WTERMSIG(status[0]);
+		if(WIFSIGNALED(status[1]))
+		{
+			tool->signal = WTERMSIG(status[1]);
+			return (tool->signal + 128);
+		}
+		if (WIFSIGNALED(status[0]) && !WIFSIGNALED(status[1]) && WTERMSIG(status[0]) == SIGQUIT)
+			tool->signal = -3;
+		tool->inside_pipe = 0;
+		return (WEXITSTATUS(status[1]));
 	}
-	if (pids[1] > 0)
-		pid = add_to_pids_list(pid, pids[1]);
-	if (pids[1] == 0)
-	{
-		
-		(close(pipefd[1]), dup2(pipefd[0], 0));
-		(close(pipefd[0]), exit(execute_tree(tree->right, envh, tool))); // SHoud it be exit and free_ evnh
-	}
-	signal(SIGINT, SIG_IGN);
-	(close(pipefd[0]), close(pipefd[1]));
-	(waitpid(pids[0], &status[0], 0), waitpid(pids[1], &status[1], 0));
-	if(WIFSIGNALED(status[0]))
-		tool->signal = WTERMSIG(status[0]);
-	if(WIFSIGNALED(status[1]))
-	{
-		tool->signal = WTERMSIG(status[1]);
-		return (tool->signal + 128);
-	}
-	if (WIFSIGNALED(status[0]) && !WIFSIGNALED(status[1]) && WTERMSIG(status[0]) == SIGQUIT)
-		tool->signal = -3;
-	tool->inside_pipe = 0;
-	return (WEXITSTATUS(status[1]));
+	return (1);
 }
 
 int	execute_tree(t_tree *tree, t_env **envh, t_tool	*tool)
